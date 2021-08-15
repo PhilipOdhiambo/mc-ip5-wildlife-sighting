cf62d582008d712089d2ef0165d078dc13c67873
Philip Odhiambo <jofonowino@gmail.com>
2021-08-16 02:16:43 +0300

    overide equals and hashCode for Animal class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,5 @@
+import com.google.common.base.Objects;
+
 public class Animal {
     private int id;
     private String name;
@@ -21,5 +23,21 @@ public class Animal {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id, this.name);
+    }
+
+    @Override
+    public boolean equals(Object anotherAnimal) {
+        if (!(anotherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal animal = (Animal) anotherAnimal;
+            this.getName().equals(animal.getName());
+        }
+
     }
 }
