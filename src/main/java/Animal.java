1a52f1b344a5d02953edd75eb4b4e364683cccf3
Philip Odhiambo <jofonowino@gmail.com>
2021-08-16 00:54:48 +0300

    add geter and setter

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -5,4 +5,20 @@ public class Animal {
     public Animal(String name) {
 
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
