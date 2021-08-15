8775408a077c8e9a377bbdc3486c583f15156b33
Philip Odhiambo <jofonowino@gmail.com>
2021-08-15 23:11:56 +0300

    add database connection file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class DB {
+import org.sql2o.Sql2o;
+
+public class DB {
+    Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker",
+            "philip", "1234");
 }
