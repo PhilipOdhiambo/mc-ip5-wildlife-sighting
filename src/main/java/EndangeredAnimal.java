d66eb9b3032a4b234703cf60786786475f19fe81
Philip Odhiambo <jofonowino@gmail.com>
2021-08-16 01:50:46 +0300

    add constants to track health and age of the endangered species

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,4 +1,20 @@
 public class EndangeredAnimal extends Animal {
+    private String age;
+    private String health;
+
+    // Health parameters
+    public static final String HEALTHY = "Healthy";
+    public static final String ILL = "Ill";
+    public static final String OK = "Okay";
+
+    // Age parameters
+    public static final String NEW_BORN = "New born";
+    public static final String YOUNG = "young";
+    public static final String ADULT = "Adult";
+
+
+    public final static String DATABASE_TYPE = "Endangered";
+
     EndangeredAnimal(String name, String age, String health) {
         super(name);
 
