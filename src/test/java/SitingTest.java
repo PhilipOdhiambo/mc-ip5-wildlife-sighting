78bf324f4e125a528f20a6b190421deb7da5ddae
Philip Odhiambo <jofonowino@gmail.com>
2021-08-16 05:59:20 +0300

    instance of siting passed

--- src/test/java/SitingTest.java
+++ src/test/java/SitingTest.java
@@ -1,8 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SitingTest extends DatabaseRule {
     @Test
+    public void siting_returnInstanceSiting_siting(){
+        Siting siting = new Siting(1,1,"zone1","philip");
+        Assertions.assertTrue(siting instanceof Siting);
+    }
 
 }
