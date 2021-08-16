fcfe3596018696d4ed965bd6918244b5d0864145
Philip Odhiambo <jofonowino@gmail.com>
2021-08-16 05:48:32 +0300

    added licence and readme

---
+++ src/main/java/Siting.java
@@ -0,0 +1,10 @@
+public class Siting {
+    private int id;
+    private int animalId;
+    private String location;
+    private String ranger;
+
+    Siting(int id, int animalId, String location, String ranger){
+
+    }
+}
